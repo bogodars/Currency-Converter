a86463fb71d893a22bf55615157e82e271a34906
bogodars <bogodar@gmail.com>
2020-02-12 15:02:11 +0200

    Updates

---
+++ precache-manifest.f2be99a5befab850ad4387a1cee8c04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee567f78468e837c72bae43dd3194dbf",
+    "url": "/Currency-Converter/index.html"
+  },
+  {
+    "revision": "98673fd28ab07734e85d",
+    "url": "/Currency-Converter/static/css/main.6b70bc88.chunk.css"
+  },
+  {
+    "revision": "5e879228ba762e40d329",
+    "url": "/Currency-Converter/static/js/2.0d43bd97.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Currency-Converter/static/js/2.0d43bd97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98673fd28ab07734e85d",
+    "url": "/Currency-Converter/static/js/main.218f85ea.chunk.js"
+  },
+  {
+    "revision": "3143aacb411ca5af3bbe",
+    "url": "/Currency-Converter/static/js/runtime-main.97fa489e.js"
+  }
+]);
