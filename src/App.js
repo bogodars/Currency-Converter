2ea9bd4e9958d3e3aebbf27b2ae80cce6c0cfd2a
bogodars <bogodar@gmail.com>
2020-02-12 11:31:34 +0200

    Styling added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import './CurrencyRow';
+import CurrencyRow from './CurrencyRow';
 
 function App() {
   return (
+		<>
    <h1>Hello World</h1>
-  );
+	 <CurrencyRow />
+	 <div className="equals">=</div>
+	 <CurrencyRow />
+  </>
+	);
 }
 
 export default App;
